215eace1de42d62270e032b8a74353eeeafc8261
Marshal <marshal.wu@qq.com>
2018-04-11 20:31:30 +0800

    add push function

--- app.mjs
+++ app.mjs
@@ -2,6 +2,8 @@ import Koa from 'koa'
 import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
 import Store from './db'
+import webpush from 'web-push'
+import Config from 'config'
 
 const app = new Koa()
 app.use(bodyParser())
@@ -13,12 +15,21 @@ router.all('/', ctx => {
 })
 
 
-router.all('/subscribe', ctx => {
+router.post('/subscribe', ctx => {
     console.log(ctx.request.body)
     let subscription = ctx.request.body
     Store.save(subscription).then(() => {
     })
     ctx.body = 'Subscribe OK.'
+})
+
+const {publicKey, privateKey} = Config.vapidDetails
+webpush.setVapidDetails(Config.mailTo, publicKey, privateKey)
+
+router.post('/push', ctx => {
+    console.log(ctx.request.body)
+    let news = ctx.request.body
+    ctx.body = 'Push OK.'
 })
 
 app.use(router.routes())
